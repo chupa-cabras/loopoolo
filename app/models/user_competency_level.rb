71ca5858b207b180ebe708806395a00880ee90e5
Chupacabras <dayvison.w.pellegrina@gmail.com>
2018-06-07 20:59:38 -0300

    Wip update form cpompetence

--- app/models/user_competency_level.rb
+++ app/models/user_competency_level.rb
@@ -2,4 +2,8 @@ class UserCompetencyLevel < ApplicationRecord
   belongs_to :user
   belongs_to :competency
   belongs_to :level
+
+  validates :user, presence: true
+  validates :competency, presence: true
+  validates :level, presence: true
 end
